5d6454ffe327e199f6fda359f0b8b9e6b452e8f0
gregori <undeadai78@gmail.com>
2026-05-09 17:30:06 +0200

    Fix Navbar/Footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,7 @@
 "use client";
 
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+
 
 export default function RootLayout({ children }) {
   return (
